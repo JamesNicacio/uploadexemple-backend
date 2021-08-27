3d236c783fb32be569e38985b1269b2dd1ea97b3
Silva <jamesson.n.silva@accenture.com>
2021-08-27 09:03:38 -0300

    Configura porta do Heroku

--- src/index.js
+++ src/index.js
@@ -30,4 +30,6 @@ app.use(
 
 app.use(require("./routes"));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
